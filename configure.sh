4a396c8f00a03625b44a0f9dc9af54035fea406d
sea-kg <mrseakg@gamail.com>
2013-05-20 02:16:59 +0700

    fixed configure.sh

--- configure.sh
+++ configure.sh
@@ -1,12 +1,20 @@
-!#/bin/bash
+#!/bin/bash
 
 ## removing old data
-
-rm -rf ../../project_security
-mkdir ../../project_security
+rm -rf project_security
+mkdir  project_security
 
 ## coping new data
-cp -rf ais314hach/* ../../project_security/
+FILE1=$(echo "$(pwd)/$1/ais314hach/*");
+
+cp -rf $FILE1 project_security
 
 ## configure
-chmod 777 -R ../project_security/*
+chmod 777 -R project_security/*
+
+#copy config file if exists
+if [ -f config.php ]; then
+	cp -rf config.php project_security/config.php
+else
+	cp project_security/config.php config.php
+fi
